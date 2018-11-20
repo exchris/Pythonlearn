dd307f712dcf732d7ce3324ef449c2c8113e99da
exchris <dev.erxuan@gmail.com>
2018-11-20 16:05:02 +0800

    python字符串与列表的相互转换

---
+++ Basic/list/list_convert_string.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+# -*- coding:utf-8 -*-
+
+# 1.字符串转列表
+str1 = "hi hello world"
+print(str1.split(" "))
+# 输出 ['hi','hello','world']
+
+# 2.列表转字符串
+l = ['hi','hello','world']
+print(" ".join(l))
+# 输出 hi hello world
