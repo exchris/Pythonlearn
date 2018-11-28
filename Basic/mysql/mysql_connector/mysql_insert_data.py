8dcc847ac47da1c80216a5de253a5bb89e5a5613
exchris <dev.erxuan@gmail.com>
2018-11-28 17:04:49 +0800

    add table data

---
+++ Basic/mysql/mysql_connector/mysql_insert_data.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python
+# -*- coding:utf-8 -*-
+
+import mysql.connector
+
+mydb = mysql.connector.connect(
+    host="127.0.0.1",
+    user="root",
+    passwd="root",
+    database="runoob"
+)
+
+mycursor = mydb.cursor()
+
+sql = "INSERT INTO sites(name, url)VALUES(%s, %s)"
+val = ("RUNOOB", "https://www.runoob.com")
+mycursor.execute(sql, val)
+
+mydb.commit()  # 数据表内容有更新，必须使用到该语句
+print(mycursor.rowcount, "记录插入成功。")
