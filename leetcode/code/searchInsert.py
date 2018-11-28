6136e89a72ae567d6d6d31edbed6b38036087b35
exchris <dev.erxuan@gmail.com>
2018-11-28 14:08:27 +0800

    leetcode 35 插入搜索位置

--- leetcode/code/searchInsert.py
+++ leetcode/code/searchInsert.py
@@ -1,2 +1,14 @@
 #!/usr/bin/python
 # -*- coding:utf-8 -*-
+class Solution:
+
+    def searchInsert(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        """
+        nums.insert(0, target)
+        nums.sort()
+        num = nums.index(target)
+        return num
