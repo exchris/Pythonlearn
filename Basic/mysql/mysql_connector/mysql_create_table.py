cd06cab8dd05bf371530e446ba5be07a40a01688
exchris <dev.erxuan@gmail.com>
2018-11-28 17:00:53 +0800

    alter table add auto_increment

---
+++ Basic/mysql/mysql_connector/mysql_create_table.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python
+# -*- coding:utf-8 -*-
+
+import mysql.connector
+
+db = mysql.connector.connect(
+    host="127.0.0.1",
+    user="root",
+    passwd="root",
+    database="runoob"
+)
+
+mycursor = db.cursor()
+mycursor.execute("use runoob")
+mycursor.execute("CREATE TABLE IF NOT EXISTS sites(name varchar(255), url varchar(255))")
+# 主键设置
+mycursor.execute("ALTER TABLE sites ADD COLUMN id INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY")
