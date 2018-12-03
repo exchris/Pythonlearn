d0359bd5b1b46618eb7e288ecc1b360390249831
exchris <dev.erxuan@gmail.com>
2018-12-03 11:32:16 +0800

    leetcode 202.快乐数

--- leetcode/code/isHappy.py
+++ leetcode/code/isHappy.py
@@ -1,3 +1,17 @@
 #!/usr/bin/python
 # -*- coding:utf-8 -*-
 __author__ = 'exchris'
+
+
+class Solution:
+    def __init__(self):
+        self.base = {4, 16, 37, 58, 145, 42, 20}
+
+    def isHappy(self, n):
+        sums = sum(map(lambda x: x ** 2, map(int, str(n))))
+        if sums in self.base:
+            return False
+        if sums != 1:
+            return self.isHappy(sums)
+        else:
+            return True
