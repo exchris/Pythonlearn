1fd120a13bacf7fff8d55ab6900cb7851cb139a7
exchris <dev.erxuan@gmail.com>
2019-01-09 17:26:06 +0800

    python100 列表排序与连接

---
+++ leetcode/hundred/drawing_56.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python
+# -*- coding:utf-8 -*-
+__author__ = 'exchris'
+
+# 画图，学用circle画图形
+
+if __name__ == '__main__':
+    # from Tkinter import *
+
+    # canvas = Canvas(width=800, height=600, bg='yellow')
+    pass
