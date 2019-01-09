65eff49b3a86d887a2ad35090b1928ed49467f39
exchris <dev.erxuan@gmail.com>
2019-01-09 17:43:54 +0800

    python100 俩字符串连接程序

---
+++ leetcode/hundred/string_join_string_86.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+# -*- coding:utf-8 -*-
+__author__ = 'exchris'
+
+# 两个字符串连接程序
+
+if __name__ == '__main__':
+    a = "acegikm"
+    b = "bdfhjlnpq"
+
+    # 连接字符串
+    c = a + b
+    print(c)
