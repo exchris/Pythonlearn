322aeecafed1ca26adc417a66f5dddb129bcfb03
exchris <dev.erxuan@gmail.com>
2019-01-09 17:42:07 +0800

    python100 循环输出列表

---
+++ leetcode/hundred/circle_output_list_77.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python
+# -*- coding:utf-8 -*-
+__author__ = 'exchris'
+
+# 循环输出列表
+
+if __name__ == '__main__':
+    s = ["man", "woman", "girl", "boy", "sister"]
+    for i in range(len(s)):
+        print(s[i])
