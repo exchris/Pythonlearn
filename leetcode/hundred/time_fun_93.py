b2d45f383d07f9082c3ec75a9e28149e79ff7f7c
exchris <dev.erxuan@gmail.com>
2019-01-09 16:45:11 +0800

    python100 时间函数举例3

---
+++ leetcode/hundred/time_fun_93.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python
+# -*- coding:utf-8 -*-
+__author__ = 'exchris'
+
+# 时间函数举例3
+
+if __name__ == '__main__':
+
+    import time
+
+    start = time.clock()
+    for i in range(10000):
+        print(i)
+    end = time.clock()
+    print('different is %6.3f' % (end - start))
