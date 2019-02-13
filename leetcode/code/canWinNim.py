6f03f26c5719f2f7e77f3df986bf5eae8349774b
exchris <dev.erxuan@gmail.com>
2019-02-13 13:04:45 +0800

    leetcode 292. Nim游戏

---
+++ leetcode/code/canWinNim.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python
+# -*- coding:utf-8 -*-
+__author__ = 'exchris'
+
+class Solution:
+    def canWinNim(self, n):
+        """
+        :type n: int
+        :rtype: bool
+        """
+        return n % 4 != 0
